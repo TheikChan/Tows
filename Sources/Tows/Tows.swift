97172efe844c56bb64bd3b1ef2bca3fa54cc0827
theikchan <theikchan.tc@gmail.com>
2022-09-14 20:48:57 +0630

    add user name and email address input field

--- Sources/Tows/Tows.swift
+++ Sources/Tows/Tows.swift
@@ -1,22 +1,41 @@
 import SwiftUI
 
+struct ContentView: View {
+    @State var name: String = ""
+    @State var email: String = ""
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                HStack{
+                    Text("User name:")
+                    TextField("",text: $name)
+                }
+                HStack{
+                    Text("Email address:")
+                    TextField("",text: $email)
+                }
+            }
+        }
+    }
+}
+
 @main
-struct HelloWorld: App {
+struct Tows: App {
   
   init() {
     DispatchQueue.main.async {
       NSApp.setActivationPolicy(.regular)
       NSApp.activate(ignoringOtherApps: true)
       NSApp.windows.first?.makeKeyAndOrderFront(nil)
-    }    
+    }
   }
   
   var body: some Scene {
-    WindowGroup { 
-      VStack { 
-          Text("Hello World!").padding()
-          Text("Enter name:").padding()
-      }      
+    WindowGroup {
+      ContentView()
+        .frame(minWidth: 640, minHeight: 320)
     }
+    .windowStyle(HiddenTitleBarWindowStyle())
   }
 }
